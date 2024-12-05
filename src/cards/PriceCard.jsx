03400e6e3599f2c6fd16581f4d6f5d5513b253d6
Md Mirza Shihab <msmd2@graduate.utm.my>
2024-12-05 17:09:02 +0600

    Designed price card with dual layer card

--- src/cards/PriceCard.jsx
+++ src/cards/PriceCard.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const PriceCard = ({ location, averagePrice , productCount, maxPrice, minPrice}) => {
+const PriceCard = ({
+  location,
+  averagePrice,
+  productCount,
+  maxPrice,
+  minPrice,
+}) => {
   return (
-    <div className="bg-earthy-beige shadow-lg rounded-lg p-5 w-64 h-56 flex flex-col items-center justify-between border border-earthy-tan">
-      <h3 className="text-xl font-semibold text-earthy-brown">{location}</h3>
-      <p className="text-2xl font-bold text-forest mt-2">৳ {averagePrice}</p>
-      <p className="text-sm text-earthy-brown mt-1">Average Price</p>
-      
-      <div className="mt-4 text-sm text-earthy-brown">
-        <p><strong>Number of Products:</strong> {productCount}</p>
-        <p> <strong>Max Price:</strong> {maxPrice}</p>
-        <p><strong>Min Price:</strong> {minPrice}</p>
+    <div className="flex items-center justify-center">
+      <div className="bg-earthy-beige shadow-lg rounded-2xl p-5 flex flex-col items-center border-8 border-earthy-tan">
+        <h3 className="text-xl font-semibold text-earthy-brown">{location}</h3>
+        <p className="text-2xl font-bold text-forest mt-2">৳ {averagePrice}</p>
+        <p className="text-sm text-earthy-brown mt-1">Average Price</p>
+
+      </div>
+
+      <div className="rounded-e-2xl bg-earthy-tan shadow-lg ">
+
+      <div className="p-6 text-sm  text-earthy-brown">
+          <p>
+            <strong>Number of Items:</strong> {productCount}
+          </p>
+          <p>
+            {" "}
+            <strong>Max Price:</strong> {maxPrice}
+          </p>
+          <p>
+            <strong>Min Price:</strong> {minPrice}
+          </p>
+        </div>
       </div>
     </div>
   );
